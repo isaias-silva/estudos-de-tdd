97e28a2d2aebd235bb84b6a8f99dfa1ee8d01f6e
isaias-silva <isaiasgarraeluta@gmail.com>
2024-10-05 17:05:38 -0300

    feat:adicionando projeto

---
+++ src/main/java/org/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package org.calculator;
+
+public class Calculator {
+    public int sum(int valueA,int valueB){
+        return valueA+valueB;
+    }
+    public int sub(int valueA,int valueB){
+        return valueA-valueB;
+    }
+    public float divide(int valueA,int valueB){
+        return (float) valueA /valueB;
+    }
+    public float multiple(int valueA,int valueB){
+        return (float) valueA * valueB;
+    }
+}
