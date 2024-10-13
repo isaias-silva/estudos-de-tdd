f5882d2f4318de146727318abd89ac211ee049c1
isaias-silva <isaiasgarraeluta@gmail.com>
2024-10-13 11:24:08 -0300

    outros throws

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,6 +6,6 @@ public class main {
 
     public static void main(String[] args) throws IOException {
         Calculator calculator= new Calculator();
-        System.out.println(calculator.processExpression("100*20/2"));
+        System.out.println(calculator.processExpression("+1"));
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,16 +4,19 @@ import org.calculator.Calculator;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.EmptyStackException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
 
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
     @Test
     public void shouldSum() {
         int valueOne = 1;
         int valueTwo = 2;
-        Calculator calculator = new Calculator();
+
         int sum = calculator.sum(valueOne, valueTwo);
         assertEquals(3, sum);
 
@@ -23,7 +26,7 @@ public class CalculatorTest {
     public void shouldSub() {
         int valueOne = 2;
         int valueTwo = 1;
-        Calculator calculator = new Calculator();
+
         int sub = calculator.sub(valueOne, valueTwo);
         assertEquals(1, sub);
 
@@ -33,7 +36,7 @@ public class CalculatorTest {
     public void shouldDivide() {
         float valueOne = 4f;
         float valueTwo = 2f;
-        Calculator calculator = new Calculator();
+
         float divide = calculator.divide(valueOne, valueTwo);
         assertEquals(2, divide, 0.02);
 
@@ -42,17 +45,17 @@ public class CalculatorTest {
     @Test
     public void shouldMultiple() {
         float valueOne = 4f;
-        float valueTwo = 0f;
-        Calculator calculator = new Calculator();
+        float valueTwo = 2f;
+
         float multiply = calculator.multiple(valueOne, valueTwo);
-        assertEquals(0, multiply, 0.02);
+        assertEquals(8, multiply, 0.02);
 
     }
 
     @Test
     public void shouldResolveMathExpression() throws IOException {
         String expressionMock = "(1+2)*3";
-        Calculator calculator = new Calculator();
+
         float resultExpression = calculator.processExpression(expressionMock);
         assertEquals(9, resultExpression, 2);
 
@@ -61,7 +64,14 @@ public class CalculatorTest {
     @Test(expected = UnsupportedOperationException.class)
     public void shouldErrorInDivisionZero() {
         String expressionMock = "(10+3)/0";
-        Calculator calculator = new Calculator();
+
+        calculator.processExpression(expressionMock);
+
+    }
+    @Test(expected = EmptyStackException.class)
+    public void shouldErrorEmptyStack() {
+        String expressionMock = "+1";
+
         calculator.processExpression(expressionMock);
 
     }
